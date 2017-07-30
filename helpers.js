859159b67da1a9cfc16fbf1a8f81bad62a538928
dbl4k <github@dbl4k.net>
2017-07-30 23:22:12 +0100

    fixed bug with null check + introduced exception

--- helpers.js
+++ helpers.js
@@ -2,11 +2,11 @@ var Helpers = function (){
 
     // Remove insecure characters that aren't part of a filename.
     this.removeRiskyChars = function(value) {
-        if(value!= isNaN()) {
+        if(value == null) {
             return value.replace(/[^\.0-9A-Za-z_-\s]/g, '');
         } else {
             // TODO : need to thow an exception here not return null..
-           return null;
+           throw new Error("value is null, terminating.");
         }  
     }
 }
