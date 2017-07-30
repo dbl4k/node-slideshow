be3b9c8da528c6f1065d7b33bd1710a12a032af6
dbl4k <github@dbl4k.net>
2017-07-30 21:52:46 +0100

    add helpers module

---
+++ helpers.js
@@ -0,0 +1,14 @@
+var Helpers = function (){
+
+    // Remove insecure characters that aren't part of a filename.
+    this.getNonTraversingFileName = function(value) {
+        if(value!= isNaN()) {
+            return value.replace(/[^\.0-9A-Za-z_-\s]/g, '');
+        } else {
+            // TODO : need to thow an exception here not return null..
+           return null;
+        }  
+    }
+}
+
+module.exports = new Helpers();
