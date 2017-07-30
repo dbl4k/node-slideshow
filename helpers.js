ed573b5584ad0c73b33f4c1b19a0fcb0b28a95f9
dbl4k <github@dbl4k.net>
2017-07-30 23:36:23 +0100

    fix another bug with nullcheck

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ var Helpers = function (){
 
     // Remove insecure characters that aren't part of a filename.
     this.removeRiskyChars = function(value) {
-        if(value == null) {
+        if(value != null) {
             return value.replace(/[^\.0-9A-Za-z_-\s]/g, '');
         } else {
             // TODO : need to thow an exception here not return null..
